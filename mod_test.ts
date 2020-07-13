417e19cc8063f3b250fb129b7a401ac5440b6909
Uzumachi <uzumachi02@gmail.com>
2020-07-13 16:17:47 +0300

    fix typo

--- mod_test.ts
+++ mod_test.ts
@@ -4,7 +4,7 @@ import * as mod from "./mod.ts";
 const USER_AGENT =
   "Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/83.0.4103.116 Safari/537.36";
 
-const BRAWSER = { name: "Chrome", version: "83.0.4103.116", major: "83" };
+const BROWSER = { name: "Chrome", version: "83.0.4103.116", major: "83" };
 const OS = { name: "Windows", version: "7" };
 const ENGINE = { name: "Blink", version: "83.0.4103.116" };
 const DEVICE = { vendor: undefined, model: undefined, type: undefined };
@@ -15,16 +15,16 @@ function getUAParser() {
 }
 
 test({
-  name: "parse brawser",
+  name: "parse browser",
   fn() {
     const uaParser = getUAParser();
 
     assertEquals(typeof uaParser, "object");
 
-    const brawser = uaParser.getBrowser();
+    const browser = uaParser.getBrowser();
 
-    assertEquals(typeof brawser, "object");
-    assertEquals(brawser, BRAWSER);
+    assertEquals(typeof browser, "object");
+    assertEquals(browser, BROWSER);
   },
 });
 
@@ -94,7 +94,7 @@ test({
     const result = uaParser.getResult();
 
     assertEquals(typeof result, "object");
-    assertEquals(result.browser, BRAWSER);
+    assertEquals(result.browser, BROWSER);
     assertEquals(result.os, OS);
     assertEquals(result.engine, ENGINE);
     assertEquals(result.device, DEVICE);
